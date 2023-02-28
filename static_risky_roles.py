a8f3ee4c06c06180204ba3f0f4db5ec1bc7eb540
WhiteBowlerHat <95751228+WhiteBowlerHat@users.noreply.github.com>
2023-02-28 18:23:03 +0100

    Update static_risky_roles.py

--- static_risky_roles.py
+++ static_risky_roles.py
@@ -17,7 +17,8 @@ def set_risky_roles_from_yaml(items):
             STATIC_RISKY_ROLES.append(Role(role['metadata']['name'],
                                            get_priority_by_name(role['metadata']['priority']),
                                            rules,
-                                           namespace=RISKY_NAMESPACE)
+                                           namespace=RISKY_NAMESPACE
+                                           risk=role['risk'])
                                       )
 
 with open(os.path.dirname(os.path.realpath(__file__)) + '/risky_roles.yaml', 'r') as stream:
